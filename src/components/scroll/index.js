d44f93356590896e814fe970a430055394025b4f
kexiaoli <568847239@qq.com>
2022-10-04 13:51:45 +0800

    feat: init

---
+++ src/components/scroll/index.js
@@ -0,0 +1,88 @@
+var Scroll = function ({
+        eleID,
+        scrollID,
+        targetID = null,
+        pageSize = 10,
+        field = 'id',
+        duration = 5,
+    }) {
+        this._targetElement = null;
+        this._element = document.getElementById(eleID);
+        this._scrollElement = document.getElementById(scrollID);
+        if (targetID) this._targetElement = document.getElementById(targetID);
+        this._size = pageSize;
+        this._len = 0;
+        this._ids = {};
+        this._field = field;
+        this._duration = duration;
+        this._itemWidth = 0;
+        this._itemHeight = 0;
+        this.init();
+        return this;
+    }
+Scroll.prototype.init = function () {
+    if (this._targetElement) {
+        this._targetElement.style.position = 'absolute';
+        this._targetElement.style['z-index'] = 100000;
+    }
+    this._scrollElement.style['will-change'] = 'transform';
+    this._scrollElement.style.position = 'absolute';
+    this._scrollElement.style.transform = 'translateX(0%)';
+    this._scrollElement.style['z-index'] = 9999;
+
+    // this._scrollElement.style['-webkit-transition'] = transition;
+    // this._scrollElement.style['-moz-transition'] = transition;
+    // this._scrollElement.style['-ms-transition'] = transition;
+    // this._scrollElement.style['-o-transition'] = transition;
+    // this._scrollElement.style['transition'] = transition;
+    // console.log(this._scrollElement.children)
+    this.createNoneElement();
+    let list = Array.from(this._scrollElement.children);
+    this._len = list.length;
+    // this._itemWidth = this._scrollElement.clientWidth;
+    this._itemHeight = this._scrollElement.children[0].clientHeight;
+    console.log(this._itemHeight)
+    list.forEach((item, index) => {
+        this._ids[item.dataset[this._field]] = index;
+    })
+    for (var i = 0; i < this._size; i++) {
+        this._scrollElement.innerHTML += this._scrollElement.innerHTML;
+    }
+}
+
+Scroll.prototype.createNoneElement = function () {
+    let child = document.createElement(this._scrollElement.children[0].tagName);
+    child.classList.value = this._scrollElement.children[0].classList.value;
+    console.log("tagName", this._scrollElement.children[0].classList.value)
+    Array.from(this._scrollElement.children[0].style).forEach((item, index) => {
+        child.style[item] = this._scrollElement.children[0].style[item]
+    })
+    child.innerHTML = '<div class="content-img" style="display: flex;justify-content: center;align-items: center;">很遗憾<br/>你今天不能吃饭</div>';
+    this._scrollElement.append(child);
+}
+
+Scroll.prototype.start = function (winId) {
+    let n = this._ids[winId] ? this._ids[winId] : this._ids[undefined],
+    // x = ((this._size - 1) * this._len + Number(n)) * this._itemWidth;
+    // if (this._targetElement) {
+    //     x = x - (this._targetElement.offsetLeft + this._targetElement.clientWidth / 2) + this._itemWidth / 2;
+    // }
+    // console.log('winId=' + winId, 'x=' + x);
+    // this._scrollElement.style.transform = 'translateX(0%)';
+    // this._scrollElement.style['transition-duration'] = '0ms';
+    // let handle = setTimeout(() => {
+    // this._scrollElement.style['transition-duration'] = this._duration + 's';
+    // this._scrollElement.style.transform = 'translateX(-' + x + 'px)';
+    //     clearTimeout(handle);
+    // })
+    y =((this._size - 1) * this._len + Number(n)) * this._itemHeight;
+    this._scrollElement.style.transform = 'translateY(0%)';
+    this._scrollElement.style['transition-duration'] = '0ms';
+    let handle = setTimeout(() => {
+        this._scrollElement.style['transition-duration'] = this._duration + 's';
+        this._scrollElement.style.transform = 'translateY(-' + y + 'px)';
+        clearTimeout(handle);
+    })
+}
+
+export default Scroll
---
+++ src/pages/home/index.js
@@ -0,0 +1,53 @@
+import { Component, useEffect } from 'react'
+import { View, Text, Button } from '@tarojs/components'
+import img1Src from "../img/1.jpeg";
+import img2Src from "../img/2.jpeg";
+import img3Src from "../img/3.jpeg";
+import img4Src from "../img/4.jpeg";
+import Scroll from '../../components/scroll';
+import './index.scss'
+
+function HomePage() {
+    let s = null;
+
+    useEffect(() => {
+        s = new Scroll({
+            eleID: "roulette-container-1",
+            scrollID: "item-case-goods-1",
+            // targetID: "roulette-container-1-open",
+            pageSize: 3,
+        })
+    }, []);
+
+    const onStart = () => {
+        console.log("111", s)
+        s.start(Math.floor(Math.random()*5)+1);
+
+    }
+
+    return (
+        <View className='index'>
+          <div className="roulette-container" id="roulette-container-1">
+            <ul id="item-case-goods-1" className="item-case-goods">
+                <li className="item-small-case restricted" data-id="1">
+                    <img className="content-img" src={img1Src} alt="" />
+                </li>
+                <li className="item-small-case restricted" data-id="2">
+                    <img className="content-img" src={img2Src} alt="" />
+                </li>
+                <li className="item-small-case restricted" data-id="3">
+                    <img className="content-img" src={img3Src} alt="" />
+                </li>
+                <li className="item-small-case restricted" data-id="4">
+                    <img className="content-img" src={img4Src} alt="" />
+                </li>
+            </ul>
+          </div>
+          <div>
+              <Button className='start-btn' onClick={onStart}>开始</Button>
+          </div>
+        </View>
+      )
+}
+
+export default HomePage
